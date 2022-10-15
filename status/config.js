62e159f47f3cf78feb495575a945280220886886
Creeper938 <creeper512@qq.com>
2022-10-15 16:33:56 +0800

    Site updated: 2022-10-15 16:33:17)

---
+++ status/config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'GroupServer 状态',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm792864476-ec2fcb1edffc6316e877f3ab',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '官网',
+      url: 'https://www.groupserver.xyz/'
+    },
+  ],
+};
